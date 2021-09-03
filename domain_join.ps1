128a311587f43d5c8d402104244a8ba802ac80ea
digantakoner <73271190+digantakoner@users.noreply.github.com>
2021-09-03 10:22:31 +0530

    Update domain_join.ps1

--- domain_join.ps1
+++ domain_join.ps1
@@ -1,6 +1,6 @@
-﻿$domain = "apac.corpdir.net"
-$password = "infosys@2014cerner@2016" | ConvertTo-SecureString -asPlainText -Force
-$username = "$domain\agsc_rdi_A_dikoner" 
+﻿$domain = "domain-name"
+$password = "password" | ConvertTo-SecureString -asPlainText -Force
+$username = "$domain\username" 
 $credential = New-Object System.Management.Automation.PSCredential($username,$password)
 $computers = get-content "C:\computers.txt"
 foreach($computer in $computers)
